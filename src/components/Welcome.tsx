335734e8e18bf94a30990e6cc8af486e87c68525
koad <kaodonflobie@zvaniga.com>
2020-12-28 20:00:51 -0500

    Changed the welcome message to reflect the Canada eCoin version of this app.

--- src/components/Welcome.tsx
+++ src/components/Welcome.tsx
@@ -5,11 +5,9 @@ import XButton from "./XButton";
 import { Text } from "../styles/components";
 
 const updates = [
-	"Upgraded to the latest version of React Native.",
-	"Upgraded several dependencies.",
-	"Fixed a bug that would prevent the transaction list from updating it's exchange rate value when toggling fiat currencies in the Settings menu.",
-	`Fixed a bug that would prevent the qrcode from updating on the "Receive Transaction" view after specifying an amount.`,
-	`The input amount text field will now display the correct fiat unit/symbol in the "Send Transaction" view.`
+	"Cloned github://coreyphillips/moonshine",
+	"Added support for Canada eCoin.",
+	"Themed with Canada eCoin colours.",
 ];
 
 const _Welcome = ({ onClose = () => null, children = <View /> } = {}) => {
@@ -17,30 +15,38 @@ const _Welcome = ({ onClose = () => null, children = <View /> } = {}) => {
 	if (Platform.OS === "ios") useEffect(() => LayoutAnimation.easeInEaseOut());
 	return (
 		<View style={styles.container}>
-			<Image
-				style={styles.icon}
-				source={require("../assets/logo/main_icon.png")}
-			/>
 			<Text style={styles.header}>Welcome!</Text>
-			<View style={{ width: "85%" }}>
+			<View style={{ width: "100%" }}>
 				{children}
 				<Text style={[styles.subHeader, { textAlign: "center" }]}>Updates in this build include:</Text>
 				{updates.map((update, i) => <Text key={update} style={styles.text}><Text style={styles.semiBoldText}>{i+1}. </Text>{update}</Text>)}
 
-				<Text style={styles.subHeader}>Questions?</Text>
+				<Text style={styles.subHeader}>Will you join us?</Text>
 
-				<Text style={styles.text}>Never hesitate to reach out:</Text>
+				<Text style={styles.text}>Find us in our community social channels:</Text>
 				<Text
-					onPress={() => Linking.openURL("mailto:support@moonshinewallet.com?subject=Requesting Some Help").catch((e) => console.log(e))}
+					onPress={() => Linking.openURL("https://twitter.com/CanadaeCoin").catch((e) => console.log(e))}
 					style={[styles.text, { marginTop: 5 }]}
 				>
-					<Text style={styles.semiBoldText}>Email: </Text>support@moonshinewallet.com
+					<Text style={styles.semiBoldText}>Twitter: </Text>@CanadaeCoin
 				</Text>
 				<Text
-					onPress={() => Linking.openURL("https://twitter.com/moonshinewallet").catch((e) => console.log(e))}
+					onPress={() => Linking.openURL("https://discord.gg/Z8DufnQ").catch((e) => console.log(e))}
 					style={[styles.text, { marginTop: 5 }]}
 				>
-					<Text style={styles.semiBoldText}>Twitter: </Text>@moonshinewallet
+					<Text style={styles.semiBoldText}>Discord:</Text>discord.gg/Z8DufnQ
+				</Text>
+				<Text
+					onPress={() => Linking.openURL("http://keybase.io/team/CanadaeCoin").catch((e) => console.log(e))}
+					style={[styles.text, { marginTop: 5 }]}
+				>
+					<Text style={styles.semiBoldText}>Keybase: </Text>keybase.io/team/CanadaeCoin
+				</Text>
+				<Text
+					onPress={() => Linking.openURL("mailto:support@canadaecoin.foundation?subject=Requesting some help RE: the moonshine wallet.").catch((e) => console.log(e))}
+					style={[styles.text, { marginTop: 5 }]}
+				>
+					<Text style={styles.semiBoldText}>Email: </Text>support@canadaecoin.foundation
 				</Text>
 			</View>
 			<XButton style={{marginVertical: 30}} onPress={onClose} />
