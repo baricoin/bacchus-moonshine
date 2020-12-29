69b49c8693779e6fd658613fd09b7d38ca60d33c
koad <kaodonflobie@zvaniga.com>
2020-12-28 19:58:17 -0500

    Added Canada eCoin to the electrum constructor

--- src/utils/electrum/clients.js
+++ src/utils/electrum/clients.js
@@ -1,19 +1,22 @@
 class Clients {
 	constructor() {
-		this.coin = "bitcoin";
+		this.coin = "canadaecoin";
 		this.mainClient = {
+			canadaecoin: false,
 			bitcoin: false,
 			litecoin: false,
 			bitcoinTestnet: false,
 			litecoinTestnet: false
 		};
 		this.peer = {
+			canadaecoin: { port: 0, host: "", protocol: "" },
 			bitcoin: { port: 0, host: "", protocol: "" },
 			litecoin: { port: 0, host: "", protocol: "" },
 			bitcoinTestnet: { port: 0, host: "", protocol: "" },
 			litecoinTestnet: { port: 0, host: "", protocol: "" }
 		};
 		this.peers = {
+			canadaecoin: [],
 			bitcoin: [],
 			litecoin: [],
 			bitcoinTestnet: [],
