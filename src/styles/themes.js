e1923946cf28780427a07e68cbe84804bf6dcaf1
koad <koad@ecoincore.com>
2021-05-15 19:20:48 -0400

    update the theme to be less bloody,.

--- src/styles/themes.js
+++ src/styles/themes.js
@@ -1,34 +1,82 @@
 export const themes = {
 	light: {
 		mode: "light",
-		gradient:["#bd4246", "#b3292d", "#90191c", "#bd4246"],
-		transparent: "transparent",
-		background: "#FFF",
-		background2: "#FFF",
-		background3: "#efefef",
-		button: "#bd4246", //colors.mediumPurple
-		card: "#FFF",
-		card2: "#FFF",
-		card3: "#631e9a", //colors.purple
-		text: "#bd4246", //colors.purple
-		text2: "#90191c", //colors.darkPurple
-		text3: "#981b1f", //colors.lightPurple
-		PRIMARY: "#bd4246", //colors.purple
-		PRIMARY_DARK: "#90191c", //colors.darkPurple
-		lightPurple: "#981b1f",
-		white: "#FFF",
-		gray: "#efefef", //colors.lightGray
-		gray2: "#6e6e6e", //colors.darkGray
-		gray3: "#f0f0f0", //colors.gray
-		warning: "#F15B2A", //colors.red
-		uneditable: "#efefef" //colors.lightGray
+
+		gradient:["#FFFC", "#FFFC"],
+		
+		// some generic things we want to use
+
+		transparent: "transparent",			//
+		white: "#fff", 						//
+		black: "#000",
+		red: "#f00",						//
+
+		button: "#bd4246",					//
+		card: "#ddd",						// Background of the cards inside the settings page
+		input: "#efefef", 				//
+		surface: "#abc",					// Background of large surfaces like the bottom half of the main screen
+		selection: "#f0f0f0", 					//
+
+		uneditable: "#efefef", 				//
+		editable: "#efefef", 				//
+
+		text: "#000000", 					//.
+		text2: "#222", 						//
+		placeholder: "#222", 						//
+
+		gray: "#efefef", 					//
+		gray2: "#6e6e6e", 					//
+		gray3: "#e6e6e6", 					//
+		nightlight: "#214121", 				//
+		PRIMARY_DARK: "#f0191c", 			//
+		lightPurple: "#001b1f", 			//
+
+		primary: "#222", 					// app status bar color for android
+		background: "#fff", 				// Background of the bottom part of the app, here the transactions list is
+		background2: "#fefefe",				// Background of the input boxes
+		background3: "#cccccc",				//
+
+		shadow: "#90191c", 					//
+		highlight: "#bd8286",				//
+		warning: "#F15B2A", 				//
+		danger: "#EE1010",					//
+
 	},
 	dark: {
 		mode: "dark",
-		gradient: [ "#2b0809", "#6c1316", "#90191c", "#410b0d"],
+		gradient:["#000000AA", "#222222DD"],
 		transparent: "transparent",
-		background: "#82171b",
-		background2: "#90191c",
+		background: "#222222",
+		background2: "#111111",
+		background3: "#555555",
+		button: "transparent",
+		card: "#333",
+		card2: "transparent",
+		card3: "#555",
+		input: "#0009", 				//
+		editable: "#0009", 				//
+		highlight: "#999",			//
+		text: "#eee",
+		text2: "#fff",
+		text3: "#aaa",
+		primary: "#333",
+		white: "#f5f5f5",
+		gray: "#efefef",
+		gray2: "#efefef",
+		gray3: "#000000",
+		warning: "#F15B2A",
+		uneditable: "#0009", 	
+		shadow: "#444",
+		PRIMARY_DARK: "#222", //colors.darkPurple
+		danger: "#991010",
+		lightPurple: "#981b1f",
+	},
+	cdndark: {
+		mode: "dark",
+		gradient:["#40191c", "#222"],
+		transparent: "transparent",
+		background: "#222222",
+		background2: "#42171b",
 		background3: "#ad1f23",
 		button: "transparent",
 		card: "#410b0d",
@@ -37,14 +85,17 @@ export const themes = {
 		text: "#f5f5f5",
 		text2: "#f5f5f5",
 		text3: "#410b0d",
-		PRIMARY: "#413c69",
-		PRIMARY_DARK: "#90191c",
-		lightPurple: "#ec9396",
+		primary: "#40191c",
 		white: "#f5f5f5",
 		gray: "#efefef",
 		gray2: "#efefef",
 		gray3: "#6c1316",
 		warning: "#F15B2A",
-		uneditable: "#410b0d"
-	},
+		uneditable: "#410b0d",
+		highlight: "#008800",
+		shadow: "#90191c",
+		PRIMARY_DARK: "#90191c", //colors.darkPurple
+		danger: "#991010",
+		lightPurple: "#981b1f",
+	}
 };
