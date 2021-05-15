d77a3987c6259bc99ce937332f3e5232ab2fd4a6
koad <koad@ecoincore.com>
2021-05-15 18:43:40 -0400

    added more explainers

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 /**
- * @format
+
+https://reactnative.dev/docs/appregistry
+AppRegistry is the JS entry point to running all React Native apps. App root components should register themselves with 
+AppRegistry.registerComponent, then the native system can load the bundle for the app and then actually run the app when 
+it's ready by invoking
+
+
  */
 
 import {AppRegistry} from 'react-native';
