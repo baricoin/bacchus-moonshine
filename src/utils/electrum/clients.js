7b08a1f171a66f6ca6945d3013ec0391b6575774
asuka431 <61589469+asuka431@users.noreply.github.com>
2022-04-01 22:38:54 +0900

    remove old unuse coins

--- src/utils/electrum/clients.js
+++ src/utils/electrum/clients.js
@@ -3,25 +3,20 @@ class Clients {
 		this.coin = "baricoin";
 		this.mainClient = {
 			baricoin: false,
-			auroracoin: false,
-			egulden: false,
 			bitcoin: false,
-			litecoin: false
+			litecoin: false,
+			litecoin: false,
 
 		};
 		this.peer = {
 			baricoin: { port: 0, host: "", protocol: "" },
-			auroracoin: { port: 0, host: "", protocol: "" },
-			egulden: { port: 0, host: "", protocol: "" },
 			bitcoin: { port: 0, host: "", protocol: "" },
-			litecoin: { port: 0, host: "", protocol: "" }
+			litecoin: { port: 0, host: "", protocol: "" },
 		};
 		this.peers = {
 			baricoin: [],
-			auroracoin: [],
-			egulden: [],
 			bitcoin: [],
-			litecoin: []
+			litecoin: [],
 		};
 	}
 	
